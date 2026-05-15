f99179c9ff9ed1f910d27fb06a660ac07c05c2b2
Zuzanna Szymerkowska <zuza.szymerkowska12@gmail.com>
2026-05-15 14:30:39 +0200

    Add Currency, Asset and ExchangeRate models

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Currency, Asset, ExchangeRate
 
-# Register your models here.
+admin.site.register(Currency)
+admin.site.register(Asset)
+admin.site.register(ExchangeRate)
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Currency(models.Model):
+    code = models.CharField(max_length=3, unique=True)  # np. USD, EUR, CHF
+    name = models.CharField(max_length=50)
+    table = models.CharField(max_length=1, default='A') # Tabela NBP (A lub B)
+
+    def __clstr__(self):
+        return f"{self.name} ({self.code})"
+
+class Asset(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='assets')
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=15, decimal_places=4) # Ilość posiadanej waluty
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.amount} {self.currency.code} - {self.user.username}"
+
+class ExchangeRate(models.Model):
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    rate = models.DecimalField(max_digits=10, decimal_places=4)
+    date = models.DateField()
+    last_updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('currency', 'date') # Jeden kurs na jeden dzień dla danej waluty
