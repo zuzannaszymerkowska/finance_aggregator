9042a7c8a1e41ee2d56a353610ee4a8584d45a6e
Zuzanna Szymerkowska <zuza.szymerkowska12@gmail.com>
2026-05-15 17:58:20 +0200

    fix: update tests to match current Currency model

--- portfolio/tests.py
+++ portfolio/tests.py
@@ -1,15 +1,18 @@
 from django.test import TestCase
-from portfolio.models import Currency, Asset
 from django.contrib.auth.models import User
-from decimal import Decimal
+from .models import Currency, Asset
 
 class PortfolioTest(TestCase):
     def setUp(self):
-        # Tworzymy użytkownika
-        self.user = User.objects.create_user(username='testuser', password='password')
-        # TWORZYMY WALUTĘ - to jest kluczowe!
-        self.usd = Currency.objects.create(code='USD', name='Dolar', table='A')
+        self.user = User.objects.create_user(username='testuser', password='password123')
+        # Usuwamy 'table', bo model go nie posiada (lub nazywa się inaczej)
+        self.usd = Currency.objects.create(code='USD', name='Dolar') 
 
     def test_add_asset(self):
-        asset = Asset.objects.create(user=self.user, currency=self.usd, amount=Decimal('100.00'))
-        self.assertEqual(asset.amount, Decimal('100.00'))
+        self.client.login(username='testuser', password='password123')
+        response = self.client.post('/add/', {
+            'currency': self.usd.id,
+            'amount': '100.00'
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Asset.objects.count(), 1)
